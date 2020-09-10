af1a98ff314bb590681d380f0d588f88f59df567
Kendra Little <littlekendra@gmail.com>
2020-09-10 14:59:25 +0100

    yolo

---
+++ Migrations/001_veryimportantchange.sql
@@ -0,0 +1,9 @@
+﻿-- <Migration ID="a3c073f9-8e66-45ca-b5e5-43482995aa90" />
+GO
+
+PRINT N'Altering [dbo].[Categories]';
+GO
+ALTER TABLE [dbo].[Categories]
+ADD [NewCol1] [NCHAR](10) NULL,
+    [FunyName] [NCHAR](10) NULL;
+GO
--- Schema-Model/dbo/Tables/Categories.sql
+++ Schema-Model/dbo/Tables/Categories.sql
@@ -3,7 +3,9 @@ CREATE TABLE [dbo].[Categories]
 [CategoryID] [int] NOT NULL IDENTITY(1, 1),
 [CategoryName] [nvarchar] (15) NOT NULL,
 [Description] [ntext] NULL,
-[Picture] [image] NULL
+[Picture] [image] NULL,
+[NewCol1] [nchar] (10) NULL,
+[FunyName] [nchar] (10) NULL
 )
 GO
 ALTER TABLE [dbo].[Categories] ADD CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED  ([CategoryID])
